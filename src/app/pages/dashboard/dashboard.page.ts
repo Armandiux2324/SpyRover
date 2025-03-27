8122ef3e3c8c5df9dfe6e0b4d785c59194496a5c
Armando <armandocandelasalvarado@gmail.com>
2025-03-27 12:33:53 -0600

    botones habilitados y deshabilitados según estado del rover

--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,31 +9,71 @@ import { Router } from '@angular/router';
   styleUrls: ['./dashboard.page.scss'],
   standalone: false
 })
-export class DashboardPage implements OnInit {
+export class DashboardPage implements OnInit, OnDestroy {
   mode: string = 'manual';
   esp32BaseUrl = 'http://192.168.18.62'
+  isRoverOnline: boolean = false;
+  private statusInterval: any;
 
-  constructor(private router:Router, private http:HttpClient) { }
+  constructor(private router:Router, private http:HttpClient, private toastCtrl: ToastController) { }
 
   ngOnInit() {
     this.setInitialMode();
+    this.checkRoverStatus();
+    this.setupStatusChecker();
+  }
+
+  ngOnDestroy() {
+    if (this.statusInterval) {
+      clearInterval(this.statusInterval);
+    }
+  }
+
+  private setupStatusChecker() {
+    this.statusInterval = setInterval(() => {
+      this.checkRoverStatus();
+    }, 5000); 
+  }
+
+  private async checkRoverStatus() {
+    try {
+      await this.http.get(this.esp32BaseUrl + '/status', { responseType: 'text' }).subscribe({
+        next: (res) => {
+          this.presentToast('Rover conectado', 'top', 'success');
+          this.isRoverOnline = true;
+        },
+        error: (err) => {
+          this.presentToast('Rover no detectado', 'top', 'danger');
+          console.error('Error al verificar el estado del rover:', err);  
+          this.isRoverOnline = false;
+        }
+      });
+      
+    } catch (error) {
+      this.isRoverOnline = false;
+    }
   }
 
   setInitialMode() {
-    this.http.get(`${this.esp32BaseUrl}/set_mode?mode=manual`).subscribe();
+    if (this.isRoverOnline) {
+      this.http.get(`${this.esp32BaseUrl}/set_mode?mode=manual`).subscribe();
+    }
   }
 
   onModeChange(mode: string) {
+    if (!this.isRoverOnline) {
+      this.presentToast('Rover desconectado', 'top', 'danger');
+      return;
+    }
+
     this.http.get(`${this.esp32BaseUrl}/set_mode?mode=${mode}`).subscribe({
       next: () => {
-        console.log(`Modo cambiado a ${mode}`);
-        // Forzar actualización visual
-        this.mode = mode; 
+        this.mode = mode;
+        this.presentToast('Modo cambiado a ' + mode, 'top', 'warning');
       },
       error: (err) => {
         console.error('Error cambiando modo:', err);
-        // Revertir cambio en caso de error
-        this.mode = this.mode === 'manual' ? 'automatic' : 'manual'; 
+        this.presentToast('Error cambiando de modo', 'top', 'danger');
       }
     });
   }
@@ -51,6 +92,16 @@ export class DashboardPage implements OnInit {
 
   redirectToHistory(){
     this.router.navigate(['/history']);
+  }
+
+  async presentToast(message: string, position: 'bottom' | 'top' | 'middle', color: 'success' | 'danger' | 'warning') {
+    const toast = await this.toastCtrl.create({
+      message: message,
+      duration: 1500,
+      position: position,
+      color: color
+    });
+    await toast.present();
   }
 
 }
--- src/app/pages/history/history.page.ts
+++ src/app/pages/history/history.page.ts
@@ -1,4 +1,8 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { DbStorageService } from 'src/app/services/db-storage.service';
+import type { OverlayEventDetail } from '@ionic/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-history',
@@ -7,10 +11,67 @@ import { Component, OnInit } from '@angular/core';
   standalone: false
 })
 export class HistoryPage implements OnInit {
+  recognitions: any[] = [];
 
-  constructor() { }
+  constructor(
+    private db: DbStorageService,
+    private toastCtrl: ToastController
+  ) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    await this.cargarHistorial();
+  }
+
+  async cargarHistorial() {
+    this.recognitions = await this.db.filter('face-recognition');
+  }
+
+  async remove(data: any) {
+    let res = await this.db.removeData(data);
+    this.recognitions = await this.db.filter('face-recognition');
+    console.log(res);
+  }
+
+  confirmDelete(data: any) {
+    this.recognitions = data;
+    const alert = document.createElement('ion-alert');
+    alert.header = 'Confirmar eliminación';
+    alert.message = '¿Estás seguro que deseas eliminar esta foto?';
+    alert.buttons = [
+      {
+        text: 'Cancelar',
+        role: 'cancel',
+        handler: () => {
+          console.log('Eliminación cancelada');
+          this.presentToast('Acción cancelada', 'top', 'danger');
+        },
+      },
+      {
+        text: 'Confirmar',
+        role: 'confirm',
+        handler: () => {
+          this.remove(data);
+          this.presentToast('Foto eliminada', 'top', 'success');
+        },
+      },
+    ];
+
+    document.body.appendChild(alert);
+    alert.present();
+  }
+
+  setResult(event: CustomEvent<OverlayEventDetail>) {
+    console.log(`Dismissed with role: ${event.detail.role}`);
+  }
+
+  async presentToast(message: string, position: 'bottom' | 'top' | 'middle', color: 'success' | 'danger' | 'warning') {
+    const toast = await this.toastCtrl.create({
+      message: message,
+      duration: 1500,
+      position: position,
+      color: color
+    });
+    await toast.present();
   }
 
 }
